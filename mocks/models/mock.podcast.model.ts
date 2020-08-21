fdcdd8b38e2d220e9570917992a51205dcf016cc
f4kinaway <afoucherpineau@protonmail.com>
2020-08-21 20:19:45 +0200

    fix mock

--- mocks/models/mock.podcast.model.ts
+++ mocks/models/mock.podcast.model.ts
@@ -2,10 +2,9 @@ import { Podcast } from '../../src/app/models/podcast.model';
 import { Episode } from 'src/app/models/episode.model';
 
 export const MockPodcast = new Podcast('', '', '', '', '', '', [
-  new Episode('', [], '', '', {
-    link: '', type: '',
-    length: 100,
-    duration: 100,
-    thumbnail: ''
-  }, '', '', new Date(), '', '', false, null, 0)
+  new Episode('', '', '', {
+    url: '',
+    type: '',
+    length: 100
+  }, '', new Date(), '', '', false, null, 0)
 ], false);
